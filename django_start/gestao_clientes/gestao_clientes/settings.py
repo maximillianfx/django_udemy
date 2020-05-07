58d75ac1e69ef98464a4af1f4b2a7813733487f3
Maximillian <maximillianfx@gmail.com>
2020-05-07 16:30:22 -0300

    Atualizacao de banco de dados para postgres

--- django_start/gestao_clientes/gestao_clientes/settings.py
+++ django_start/gestao_clientes/gestao_clientes/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestao_clientes',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'postgres_db',
+        'PORT': '5432'
     }
 }
 
